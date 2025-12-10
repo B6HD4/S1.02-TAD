198504aca9bde1a83cc29e4babf376d5eee22192
Bixente <bixe.hiriart@gmail.com>
2025-12-10 08:37:17 +0000

    modifié :         grille.h

--- grille.h
+++ grille.h
@@ -7,6 +7,22 @@ Création : 10/12/2025
 #ifndef GRILLE_H
 #define GRILLE_H
 
+struct Case {
+        char symbole;   // Symbole stocké dans la case
+        bool estCache;  // Vrai si la case est cachée, faux sinon
+};
 
+const unsigned short int TAILLE_TAB = 200;  // Taille pysique du tableau
 
-//Définition des types abstrait
+struct Grille {
+        Case matrice[TAILLE_TAB]
+                    [TAILLE_TAB];   // Matrice contenant les cases de la grille
+        unsigned short int taille;  // taille logique du tableau
+        char symboleCaseVide;  // Symbole dans la cazse est vide quand elle est
+                               // vide
+        char symbooleCaseCachee;  // Symbole affiché quand la case est cachée
+};
+
+#endif
+
+// Définition des types abstrait
