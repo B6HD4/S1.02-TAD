356dd324962fe28930e99538c82fbb5388f6d9db
Bixente <bixe.hiriart@gmail.com>
2025-12-16 17:27:30 +0000

    modifié :         grille.cpp 	modifié :         grille.h

--- grille.cpp
+++ grille.cpp
@@ -7,3 +7,51 @@
  * @date mar. 16 déc. 2025 16:42:40
  * @remarks
  */
+
+#include "grille.h"
+#include <iostream>
+
+void genererException(const string &detailException) {
+        throw std::invalid_argument(detailException);
+}
+
+short int ValUtoR(short int val) { return val - 1; }
+
+short int ValRtoU(short int val) { return val + 1; }
+
+char getSymboleCase(const Grille &grille, short int ligne, short int colonne) {
+        return grille.matrice[ValUtoR(ligne)][ValUtoR(colonne)].symbole;
+}
+
+short int getTailleGrille(const Grille &grille) { return grille.taille; }
+
+char getSymboleCaseVide(const Grille &grille) { return grille.symboleCaseVide; }
+
+char getSymboleCaseCachee(const Grille &grille) {
+        return grille.symboleCaseCachee;
+}
+
+void setCaseSymbole(Grille &grille, char symbole, short int ligne,
+                    short int colonne) {
+        grille.matrice[ligne][colonne].symbole = symbole;
+}
+
+void setCaseCachee(Grille &grille, short int ligne, short int colonne) {
+        grille.matrice[ligne][colonne].estCache = true;
+}
+
+void setCaseVisible(Grille &grille, short int ligne, short int colonne) {
+        grille.matrice[ligne][colonne].estCache = false;
+}
+
+void setTailleGrille(Grille &grille, short int taille) {
+        grille.taille = taille;
+}
+
+void setSymboleCaseVide(Grille &grille, char symbole) {
+        grille.symboleCaseVide = symbole;
+}
+
+void setSymboleCaseCachee(Grille &grille, char symbole) {
+        grille.symboleCaseCachee = symbole;
+}
