335624f0d40111af594f0b1185150cc6f53cbe61
Ced <crouille@iutbayonne.univ-pau.fr>
2025-12-16 18:46:04 +0100

    add several initGrille

--- grille.cpp
+++ grille.cpp
@@ -7,3 +7,92 @@
  * @date mar. 16 déc. 2025 16:42:40
  * @remarks
  */
+
+#include"grille.h"
+
+void initGrille(Grille &grille, short int taille, char symbole, bool estVide, char symboleCaseVide, bool estCache, char symboleCaseCachee){
+    setTailleGrille(grille, taille);
+    if (estVide)
+    {
+        for (int i = 0; i < taille-1; i++)
+        {
+            setSymboleCaseVide(grille, symboleCaseVide);
+        }
+    }    
+    else if (estCache)
+    {
+        for (int i = 0; i < taille-1; i++)
+        {
+            setSymboleCaseCachee(grille, symboleCaseCachee);
+        }
+    }
+    else if (symbole)
+    {
+        for (int i = 0; i < taille-1; i++)
+        {
+            setCaseSymbole(grille, symbole);
+        }
+    }
+    else
+        genererException("Impossible de créer la grille");
+}
+
+void initGrille(Grille &grille, short int taille, char symbole, bool estVide,char symboleCaseVide, bool estCache){
+    setTailleGrille(grille, taille);
+    if (estVide)
+    {
+        for (int i = 0; i < taille-1; i++)
+        {
+            setSymboleCaseVide(grille, symboleCaseVide);
+        }
+    }    
+    else if (estCache)
+    {
+        for (int i = 0; i < taille-1; i++)
+        {
+            setSymboleCaseCachee(grille, '.');
+        }
+    }
+    else if (symbole)
+    {
+        for (int i = 0; i < taille-1; i++)
+        {
+            setCaseSymbole(grille, symbole);
+        }
+    }
+    else
+        genererException("Impossible de créer la grille");
+}
+
+void initGrille(Grille &grille, short int taille, char symbole, bool estVide,bool estCache, char symboleCaseCachee){setTailleGrille(grille, taille);
+    if (estVide)
+    {
+        for (int i = 0; i < taille-1; i++)
+        {
+            setSymboleCaseVide(grille, '_');
+        }
+    }    
+    else if (estCache)
+    {
+        for (int i = 0; i < taille-1; i++)
+        {
+            setSymboleCaseCachee(grille, symboleCaseCachee);
+        }
+    }
+    else if (symbole)
+    {
+        for (int i = 0; i < taille-1; i++)
+        {
+            setCaseSymbole(grille, symbole);
+        }
+    }
+    else
+        genererException("Impossible de créer la grille");}
+
+void initGrille(Grille &grille, short int taille, bool estVide,char symboleCaseVide, bool estCache, char symboleCaseCachee){}
+
+void initGrille(Grille &grille, short int taille, char symbole, bool estVide,bool estCache){}
+
+void initGrille(Grille &grille, short int taille, bool estVide, bool estCache,char symboleCaseCachee){}
+
+void initGrille(Grille &grille, short int taille, bool estVide, bool estCache){}
