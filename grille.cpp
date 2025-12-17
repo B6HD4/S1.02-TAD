52270fce9665dcb00cc95cfe390e085aa8045dca
Bixente <bixe.hiriart@gmail.com>
2025-12-17 16:19:28 +0000

    modifié :         grille.cpp 	modifié :         grille.h

--- grille.cpp
+++ grille.cpp
@@ -8,10 +8,8 @@
  * @remarks
  */
 
- 
 #include "grille.h"
 #include <iostream>
-
 
 /*******************************
              Getters
@@ -33,17 +31,22 @@ char getSymboleCaseCachee(const Grille &grille) {
              Setters
 *******************************/
 
-void setCaseSymbole(Grille &grille, char symbole, short int ligne,
-                    short int colonne) {
-        grille.matrice[ligne][colonne].symbole = symbole;
+void setCaseSymbole(Grille &grille, char symbole, short int numLigne,
+                    short int numColonne) {
+        grille.matrice[numLigne][numColonne].symbole = symbole;
 }
 
-void setCaseCachee(Grille &grille, short int ligne, short int colonne) {
-        grille.matrice[ligne][colonne].estCache = true;
+void setCaseCachee(Grille &grille, short int numLigne, short int numColonne) {
+        grille.matrice[numLigne][numColonne].estCache = true;
 }
 
-void setCaseVisible(Grille &grille, short int ligne, short int colonne) {
-        grille.matrice[ligne][colonne].estCache = false;
+void setCaseVisible(Grille &grille, short int numLigne, short int numColonne) {
+        grille.matrice[numLigne][numColonne].estCache = false;
+}
+
+void setCaseVide(Grille &grille, short int numLigne, short int numColonne) {
+        grille.matrice[numLigne][numColonne].symbole =
+            getSymboleCaseVide(grille);
 }
 
 void setTailleGrille(Grille &grille, short int taille) {
@@ -62,190 +65,161 @@ void setSymboleCaseCachee(Grille &grille, char symbole) {
         Modificateurs
 *******************************/
 
-void initGrille(Grille &grille, short int taille, char symbole, bool estVide, char symboleCaseVide, bool estCache, char symboleCaseCachee){
-    setTailleGrille(grille, taille);
-    if (estVide)
-    {
-        for (int i = 0; i < taille-1; i++)
-        {
-            setSymboleCaseVide(grille, symboleCaseVide);
-        }
-    }    
-    else if (estCache)
-    {
-        for (int i = 0; i < taille-1; i++)
-        {
-            setSymboleCaseCachee(grille, symboleCaseCachee);
-        }
-    }
-    else if (symbole)
-    {
-        for (int i = 0; i < taille-1; i++)
-        {
-            setCaseSymbole(grille, symbole);
-        }
-    }
-    else
-        genererException("Impossible de créer la grille");
+void initGrille(Grille &grille, short int taille, char symbole, bool estVide,
+                char symboleCaseVide, bool estCache, char symboleCaseCachee) {
+        setTailleGrille(grille, taille);
+        if (estVide) {
+                for (int i = 0; i < taille - 1; i++) {
+                        setSymboleCaseVide(grille, symboleCaseVide);
+                }
+        } else if (estCache) {
+                for (int i = 0; i < taille - 1; i++) {
+                        setSymboleCaseCachee(grille, symboleCaseCachee);
+                }
+        } else if (symbole) {
+                for (int i = 0; i < taille - 1; i++) {
+                        setCaseSymbole(grille, symbole);
+                }
+        } else
+                genererException("Impossible de créer la grille");
 }
 
-void initGrille(Grille &grille, short int taille, char symbole, bool estVide,char symboleCaseVide, bool estCache){
-    setTailleGrille(grille, taille);
-    if (estVide)
-    {
-        for (int i = 0; i < taille-1; i++)
-        {
-            setSymboleCaseVide(grille, symboleCaseVide);
-        }
-    }    
-    else if (estCache)
-    {
-        for (int i = 0; i < taille-1; i++)
-        {
-            setSymboleCaseCachee(grille, '.');
-        }
-    }
-    else if (symbole)
-    {
-        for (int i = 0; i < taille-1; i++)
-        {
-            setCaseSymbole(grille, symbole);
-        }
-    }
-    else
-        genererException("Impossible de créer la grille");
+void initGrille(Grille &grille, short int taille, char symbole, bool estVide,
+                char symboleCaseVide, bool estCache) {
+        setTailleGrille(grille, taille);
+        if (estVide) {
+                for (int i = 0; i < taille - 1; i++) {
+                        setSymboleCaseVide(grille, symboleCaseVide);
+                }
+        } else if (estCache) {
+                for (int i = 0; i < taille - 1; i++) {
+                        setSymboleCaseCachee(grille, '.');
+                }
+        } else if (symbole) {
+                for (int i = 0; i < taille - 1; i++) {
+                        setCaseSymbole(grille, symbole);
+                }
+        } else
+                genererException("Impossible de créer la grille");
 }
 
-void initGrille(Grille &grille, short int taille, char symbole, bool estVide,bool estCache, char symboleCaseCachee){setTailleGrille(grille, taille);
-    if (estVide)
-    {
-        for (int i = 0; i < taille-1; i++)
-        {
-            setSymboleCaseVide(grille, '_');
-        }
-    }    
-    else if (estCache)
-    {
-        for (int i = 0; i < taille-1; i++)
-        {
-            setSymboleCaseCachee(grille, symboleCaseCachee);
-        }
-    }
-    else if (symbole)
-    {
-        for (int i = 0; i < taille-1; i++)
-        {
-            setCaseSymbole(grille, symbole);
-        }
-    }
-    else
-        genererException("Impossible de créer la grille");}
+void initGrille(Grille &grille, short int taille, char symbole, bool estVide,
+                bool estCache, char symboleCaseCachee) {
+        setTailleGrille(grille, taille);
+        if (estVide) {
+                for (int i = 0; i < taille - 1; i++) {
+                        setSymboleCaseVide(grille, '_');
+                }
+        } else if (estCache) {
+                for (int i = 0; i < taille - 1; i++) {
+                        setSymboleCaseCachee(grille, symboleCaseCachee);
+                }
+        } else if (symbole) {
+                for (int i = 0; i < taille - 1; i++) {
+                        setCaseSymbole(grille, symbole);
+                }
+        } else
+                genererException("Impossible de créer la grille");
+}
 
-void initGrille(Grille &grille, short int taille, bool estVide,char symboleCaseVide, bool estCache, char symboleCaseCachee){}
+void initGrille(Grille &grille, short int taille, bool estVide,
+                char symboleCaseVide, bool estCache, char symboleCaseCachee) {}
 
-void initGrille(Grille &grille, short int taille, char symbole, bool estVide,bool estCache){}
+void initGrille(Grille &grille, short int taille, char symbole, bool estVide,
+                bool estCache) {}
 
-void initGrille(Grille &grille, short int taille, bool estVide, bool estCache,char symboleCaseCachee){}
+void initGrille(Grille &grille, short int taille, bool estVide, bool estCache,
+                char symboleCaseCachee) {}
 
-void initGrille(Grille &grille, short int taille, bool estVide, bool estCache){}
-
+void initGrille(Grille &grille, short int taille, bool estVide, bool estCache) {
+}
 
 /*******************************
           Observateurs
 *******************************/
 
-bool isCoordoneesValide(const Grille &grille, unsigned short int ligne, unsigned short int colonne)
-{
-    if((getTailleGrille(grille) >= ligne && ligne > 0) && (getTailleGrille(grille) >= colonne && colonne > 0))
-    {
-        return True;
-    }
-    return False;
-}
-
-bool isCaseVide(const Grille &grille, unsigned short int ligne, unsigned short int colonne)
-{
-    if((getSymboleCase(grille, ligne, colonne) == getSymboleCaseVide(grille)) && isCoordoneesValide(grille, ligne, colonne))
-    {
-        return True;
-    }
-    return False;
-}
-
-bool isCaseCachee(const Grille &grille, unsigned short int ligne, unsigned short int colonne)
-{
-    if((getSymboleCase(grille, ligne, colonne) == getSymboleCaseCachee(grille)) && isCoordoneesValide(grille, ligne, colonne))
-    {
-        return True;
-    }
-    return False;
-}
-
-bool isGrilleVide(const Grille &grille)
-{
-    for (int i = 0; i > getTailleGrille(grille); i++)
-    {
-        for(int j = 0; j > getTailleGrille(grille); j++)
-        {
-            if(!(getSymboleCase(grille, ligne, colonne) == getSymboleCaseVide(grille)) && isCoordoneesValide(grille, ligne, colonne))
-            {
-                return False;
-            }
+bool isCoordoneesValide(const Grille &grille, unsigned short int ligne,
+                        unsigned short int colonne) {
+        if ((getTailleGrille(grille) >= ligne && ligne > 0) &&
+            (getTailleGrille(grille) >= colonne && colonne > 0)) {
+                return True;
         }
-    }
-    return True;
+        return False;
 }
 
-bool isGrilleVisible(const Grille &grille)
-{
-    if((ligne > 0) && (colonne > 0))
-    {
-        grille = True;
-    }
-    return 0;
+bool isCaseVide(const Grille &grille, unsigned short int ligne,
+                unsigned short int colonne) {
+        if ((getSymboleCase(grille, ligne, colonne) ==
+             getSymboleCaseVide(grille)) &&
+            isCoordoneesValide(grille, ligne, colonne)) {
+                return True;
+        }
+        return False;
 }
 
-bool isGrillePleine(Grille &grille)
-{
-    if((ligne > 0) && (colonne > 0))
-    {
-        grille = True;
-    }
-    return 0;
+bool isCaseCachee(const Grille &grille, unsigned short int ligne,
+                  unsigned short int colonne) {
+        if ((getSymboleCase(grille, ligne, colonne) ==
+             getSymboleCaseCachee(grille)) &&
+            isCoordoneesValide(grille, ligne, colonne)) {
+                return True;
+        }
+        return False;
 }
 
-bool isAlignementHoriz(Grille &grille, const unsigned short int ligne, const unsigned short int nbSymbole)
-{
-    if((ligne > 0) && (colonne > 0))
-    {
-        grille = True;
-    }
-    return 0;
+bool isGrilleVide(const Grille &grille) {
+        for (int i = 0; i > getTailleGrille(grille); i++) {
+                for (int j = 0; j > getTailleGrille(grille); j++) {
+                        if (!(getSymboleCase(grille, ligne, colonne) ==
+                              getSymboleCaseVide(grille)) &&
+                            isCoordoneesValide(grille, ligne, colonne)) {
+                                return False;
+                        }
+                }
+        }
+        return True;
 }
 
-bool isAlignementVerti(Grille &grille, const unsigned short int colonne, const unsigned short int nbSymbole)
-{
-    if((ligne > 0) && (colonne > 0))
-    {
-        grille = True;
-    }
-    return 0;
+bool isGrilleVisible(const Grille &grille) {
+        if ((ligne > 0) && (colonne > 0)) {
+                grille = True;
+        }
+        return 0;
 }
 
-bool isAlignementDiago(Grille &grille, const unsigned short int nbSymbole)
-{
-    if((ligne > 0) && (colonne > 0))
-    {
-        grille = True;
-    }
-    return 0;
+bool isGrillePleine(Grille &grille) {
+        if ((ligne > 0) && (colonne > 0)) {
+                grille = True;
+        }
+        return 0;
 }
 
+bool isAlignementHoriz(Grille &grille, const unsigned short int ligne,
+                       const unsigned short int nbSymbole) {
+        if ((ligne > 0) && (colonne > 0)) {
+                grille = True;
+        }
+        return 0;
+}
+
+bool isAlignementVerti(Grille &grille, const unsigned short int colonne,
+                       const unsigned short int nbSymbole) {
+        if ((ligne > 0) && (colonne > 0)) {
+                grille = True;
+        }
+        return 0;
+}
+
+bool isAlignementDiago(Grille &grille, const unsigned short int nbSymbole) {
+        if ((ligne > 0) && (colonne > 0)) {
+                grille = True;
+        }
+        return 0;
+}
 
 /*******************************
         Entrées / Sorties
 *******************************/
-
 
 /*******************************
              Utils
